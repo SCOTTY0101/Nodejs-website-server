e3310b16959d85a249305664a86d4b9dc180cd52
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2020-11-09 21:00:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //Start a nodejs website server 
+//First install nodejs and npm
 
+//http module
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//create http server that listens on port 3000
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
