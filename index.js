26642b83f7e58a5dbf5fb340f7aa11165e58afb2
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2022-06-27 21:21:31 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//Start a nodejs website server 
-//First install nodejs and npm
+/*
+*Start a nodejs website server. Text on local host at 127.0.0.1:3000  (port :3000) 
+*First install nodejs and np
+*/
 
 //http module
 const http = require("http");
@@ -7,13 +9,24 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 //create http server that listens on port 3000
+
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
 });
 
+//Start server with:  node index.js
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname} : ${port}/`);
 });
 
+/* 
+*   Use with package.json file. Double quotes when working with JSON.
+*
+*   "script" {
+*       "start" : "node index.js"
+*       }
+*
+*/ 
